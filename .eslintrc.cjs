862bfaae5b72ae9f0223c18368874cc000a9d4a8
Rizky Aditya <rizkyadit05@gmail.com>
2024-01-16 16:24:19 +0700

    [RIZKY] - config prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
   root: true,
@@ -8,5 +8,16 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
+    "prettier"
   ],
-};
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        // put conflicted rules here, below is an example
+        // singleQuote: true
+      }
+    ]
+  }
+}
